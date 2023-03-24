6617bad3fe0551672526f2d651714a819ada3323
cph-mh1088 <112551104+cph-mh1088@users.noreply.github.com>
2023-03-24 13:53:00 +0100

    update konto to saldo

--- src/main/java/dat/backend/model/persistence/UserMapper.java
+++ src/main/java/dat/backend/model/persistence/UserMapper.java
@@ -59,7 +59,7 @@ public class UserMapper {
     public static void indsætBeløb(int beløb, String brugernavn, ConnectionPool connectionPool) throws DatabaseException {
 
         Logger.getLogger("web").log(Level.INFO, "");
-        String penge = "UPDATE bruger SET konto = ? WHERE brugernavn = ?";
+        String penge = "UPDATE bruger SET saldo = ? WHERE brugernavn = ?";
 
             try (Connection connection = connectionPool.getConnection();
                  PreparedStatement statement = connection.prepareStatement(penge)) {
