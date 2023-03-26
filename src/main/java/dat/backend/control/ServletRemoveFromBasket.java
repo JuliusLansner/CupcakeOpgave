ebc3a45a0509fb5bdb274b1a7af49a42c5025003
cph-mh1088 <112551104+cph-mh1088@users.noreply.github.com>
2023-03-26 13:26:51 +0200

    start of activity diagram

--- src/main/java/dat/backend/control/ServletRemoveFromBasket.java
+++ src/main/java/dat/backend/control/ServletRemoveFromBasket.java
@@ -8,30 +8,32 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 @WebServlet(name = "ServletRemoveFromBasket", value = "/ServletRemoveFromBasket")
 public class ServletRemoveFromBasket extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    HttpSession session = request.getSession();
+        HttpSession session = request.getSession();
 
-    String order = request.getParameter("order");
-    int productId = Integer.parseInt(order);
+        String order = request.getParameter("order");
+        int productId = Integer.parseInt(order);
 
+        ArrayList<Product> products = (ArrayList<Product>) session.getAttribute("kurvindhold");
 
-    ArrayList<Product> products = (ArrayList<Product>) session.getAttribute("kurvindhold");
-
-    for(Product product:products){
-        if(product.getProductId()==productId){
-            products.remove(product);
-            ProduktFacade.deleteProductWithProductId(productId);
+        Iterator<Product> iterator = products.iterator();
+        while (iterator.hasNext()) {
+            Product product = iterator.next();
+            if (product.getProductId() == productId) {
+                iterator.remove();
+                ProduktFacade.deleteProductWithProductId(productId);
+            }
         }
+
+        session.setAttribute("kurvindhold", products);
+        request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 
-    session.setAttribute("kurvindhold",products);
-    request.getRequestDispatcher("index.jsp").forward(request,response);
-
-    }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
