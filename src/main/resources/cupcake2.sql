9d849630e47418daccc383ef9c9b93aff2aa68da
mald004 <cph-mm737@cphbusiness.dk>
2023-03-29 13:04:36 +0200

    -Opdateret database script igen

--- src/main/resources/cupcake2.sql
+++ src/main/resources/cupcake2.sql
@@ -1,44 +1,111 @@
-CREATE DATABASE `cupcake` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-CREATE TABLE `bruger` (
-  `brugernavn` varchar(45) NOT NULL,
-  `kodeord` int NOT NULL,
-  `rolle` varchar(45) NOT NULL,
-  `saldo` int NOT NULL DEFAULT '0',
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema cupcake
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema cupcake
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `cupcake` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `cupcake` ;
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`bruger`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`bruger` (
+  `brugernavn` VARCHAR(45) NOT NULL,
+  `kodeord` INT NOT NULL,
+  `rolle` VARCHAR(45) NOT NULL,
+  `saldo` INT NOT NULL DEFAULT '0',
   PRIMARY KEY (`brugernavn`),
-  UNIQUE KEY `brugernavn_UNIQUE` (`brugernavn`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE `bund` (
-  `navn` varchar(45) NOT NULL,
-  `pris` int NOT NULL,
-  PRIMARY KEY (`navn`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE `ordre` (
-  `idordre` int NOT NULL AUTO_INCREMENT,
-  `bruger` varchar(45) NOT NULL,
-  `dato` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE INDEX `brugernavn_UNIQUE` (`brugernavn` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`bund`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`bund` (
+  `navn` VARCHAR(45) NOT NULL,
+  `pris` INT NOT NULL,
+  PRIMARY KEY (`navn`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`ordre`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`ordre` (
+  `idordre` INT NOT NULL AUTO_INCREMENT,
+  `bruger` VARCHAR(45) NOT NULL,
+  `dato` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`idordre`),
-  UNIQUE KEY `idordre_UNIQUE` (`idordre`),
-  KEY `fk_ordre_bruger1_idx` (`bruger`),
-  CONSTRAINT `fk_ordre_bruger1` FOREIGN KEY (`bruger`) REFERENCES `bruger` (`brugernavn`)
-) ENGINE=InnoDB AUTO_INCREMENT=122 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE `produkt` (
-  `produktid` int NOT NULL AUTO_INCREMENT,
-  `top` varchar(45) NOT NULL,
-  `bund` varchar(45) NOT NULL,
-  `pris` int NOT NULL,
-  `ordreid` int NOT NULL,
-  `antal` int NOT NULL,
+  UNIQUE INDEX `idordre_UNIQUE` (`idordre` ASC) VISIBLE,
+  INDEX `fk_ordre_bruger1_idx` (`bruger` ASC) VISIBLE,
+  CONSTRAINT `fk_ordre_bruger1`
+    FOREIGN KEY (`bruger`)
+    REFERENCES `cupcake`.`bruger` (`brugernavn`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 122
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`top`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`top` (
+  `navn` VARCHAR(45) NOT NULL,
+  `pris` INT NOT NULL,
+  PRIMARY KEY (`navn`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `cupcake`.`produkt`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `cupcake`.`produkt` (
+  `produktid` INT NOT NULL AUTO_INCREMENT,
+  `top` VARCHAR(45) NOT NULL,
+  `bund` VARCHAR(45) NOT NULL,
+  `pris` INT NOT NULL,
+  `ordreid` INT NOT NULL,
+  `antal` INT NOT NULL,
   PRIMARY KEY (`produktid`),
-  UNIQUE KEY `produktid_UNIQUE` (`produktid`),
-  KEY `fk_produkt_top_idx` (`top`),
-  KEY `fk_produkt_bund1_idx` (`bund`),
-  KEY `fk_produkt_ordre1_idx` (`ordreid`),
-  CONSTRAINT `fk_produkt_bund1` FOREIGN KEY (`bund`) REFERENCES `bund` (`navn`),
-  CONSTRAINT `fk_produkt_ordre1` FOREIGN KEY (`ordreid`) REFERENCES `ordre` (`idordre`),
-  CONSTRAINT `fk_produkt_top` FOREIGN KEY (`top`) REFERENCES `top` (`navn`)
-) ENGINE=InnoDB AUTO_INCREMENT=69 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-CREATE TABLE `top` (
-  `navn` varchar(45) NOT NULL,
-  `pris` int NOT NULL,
-  PRIMARY KEY (`navn`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE INDEX `produktid_UNIQUE` (`produktid` ASC) VISIBLE,
+  INDEX `fk_produkt_top_idx` (`top` ASC) VISIBLE,
+  INDEX `fk_produkt_bund1_idx` (`bund` ASC) VISIBLE,
+  INDEX `fk_produkt_ordre1_idx` (`ordreid` ASC) VISIBLE,
+  CONSTRAINT `fk_produkt_bund1`
+    FOREIGN KEY (`bund`)
+    REFERENCES `cupcake`.`bund` (`navn`),
+  CONSTRAINT `fk_produkt_ordre1`
+    FOREIGN KEY (`ordreid`)
+    REFERENCES `cupcake`.`ordre` (`idordre`),
+  CONSTRAINT `fk_produkt_top`
+    FOREIGN KEY (`top`)
+    REFERENCES `cupcake`.`top` (`navn`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 69
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
